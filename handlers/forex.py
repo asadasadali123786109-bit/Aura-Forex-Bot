52aca8174e65581c0824151bae3aab00a807316e
asadasadali123786109-bit <asadasadali123786109@gmail.com>
2026-07-08 20:10:45 +0500

    Update forex.py

--- handlers/forex.py
+++ handlers/forex.py
@@ -1,35 +1,12 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+from utils.keyboard import forex_menu
+
+
 async def forex_signals(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
     await update.message.reply_text(
-        "📊 Today's Forex Signals\n\n"
-
-        "🥇 XAU/USD (Gold)\n"
-        "➡️ BUY 🟢\n"
-        "⏰ Time: 5M\n\n"
-
-        "💶 EUR/USD\n"
-        "➡️ BUY 🟢\n"
-        "⏰ Time: 5M\n\n"
-
-        "💷 GBP/USD\n"
-        "➡️ SELL 🔴\n"
-        "⏰ Time: 5M\n\n"
-
-        "🇺🇸 USD/JPY\n"
-        "➡️ BUY 🟢\n"
-        "⏰ Time: 5M\n\n"
-
-        "🇦🇺 AUD/USD\n"
-        "➡️ SELL 🔴\n"
-        "⏰ Time: 5M\n\n"
-
-        "🇺🇸 USD/CAD\n"
-        "➡️ BUY 🟢\n"
-        "⏰ Time: 5M\n\n"
-
-        "🇳🇿 NZD/USD\n"
-        "➡️ SELL 🔴\n"
-        "⏰ Time: 5M"
+        "📊 اپنا Forex Pair منتخب کریں۔",
+        reply_markup=forex_menu()
     )
