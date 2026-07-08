89167cb7a84cdc3c8fe06367f828f1762ab9dd50
asadasadali123786109-bit <asadasadali123786109@gmail.com>
2026-07-08 20:09:58 +0500

    Update keyboard.py

--- utils/keyboard.py
+++ utils/keyboard.py
@@ -12,3 +12,20 @@ def main_menu():
         keyboard,
         resize_keyboard=True
     )
+
+
+def forex_menu():
+
+    keyboard = [
+        ["🥇 XAU/USD", "💶 EUR/USD"],
+        ["💷 GBP/USD", "💴 USD/JPY"],
+        ["🇦🇺 AUD/USD", "🇺🇸 USD/CAD"],
+        ["🇳🇿 NZD/USD", "💶 EUR/JPY"],
+        ["💷 GBP/JPY", "₿ BTC/USD"],
+        ["🔙 Back"]
+    ]
+
+    return ReplyKeyboardMarkup(
+        keyboard,
+        resize_keyboard=True
+    )
