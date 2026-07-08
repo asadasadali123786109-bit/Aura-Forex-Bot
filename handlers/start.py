8e2b2f5609794d30b9ae0c728891f200bdc4a4d8
asadasadali123786109-bit <asadasadali123786109@gmail.com>
2026-07-08 19:06:25 +0500

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,20 @@
+from telegram import Update
+from telegram.ext import ContextTypes
 
+from database import add_user
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    user = update.effective_user
+
+    add_user(
+        user.id,
+        user.username,
+        user.first_name
+    )
+
+    await update.message.reply_text(
+        f"السلام علیکم {user.first_name} 👋\n\n"
+        "Aura Forex Bot میں خوش آمدید۔"
+    )
